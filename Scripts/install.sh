f8db94712ae9f19ec5a08d8efb60a35302cc2365
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2024-09-05 16:34:42 -0300

    up

--- Scripts/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo apt install -y xclip maim picom dunst kitty polybar rofi cava brightnessctl playerctl feh redshift
---
+++ Scripts/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt install -y xclip maim picom dunst kitty polybar rofi cava brightnessctl playerctl feh redshift git
+
+sudo chmod +s /usr/bin/brightnessctl
